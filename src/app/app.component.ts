f9d90ef776ba8029e800a566fd7dd6c51822e931
Iago Mauricio <iagomauricio7@gmail.com>
2025-02-06 09:50:45 -0300

    main e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { MainComponent } from "./main/main.component";
+import { FooterComponent } from "./footer/footer.component";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MainComponent],
+  imports: [RouterOutlet, NavbarComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Oceanos de Plástico';
 
-  ngOnInit(): void {
-    initFlowbite();
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      initFlowbite();
+    }
   }
 }
