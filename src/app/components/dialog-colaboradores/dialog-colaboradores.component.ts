94b9d493633ccf90a7e9a44e28b6459a64e2fd46
Iago Mauricio <iagomauricio7@gmail.com>
2025-02-12 08:31:15 -0300

    mudando a tela de soluções e contato (sem implementação ainda)

---
+++ src/app/components/dialog-colaboradores/dialog-colaboradores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-colaboradores',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-colaboradores.component.html',
+  styleUrl: './dialog-colaboradores.component.css'
+})
+export class DialogColaboradoresComponent {
+
+}
---
+++ src/app/shared/flags-navbar/flag-brazil/flag-brazil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flag-brazil',
+  standalone: true,
+  imports: [],
+  templateUrl: './flag-brazil.component.html',
+  styleUrl: './flag-brazil.component.css'
+})
+export class FlagBrazilComponent {
+
+}
