6fd55caf9ea369ac92470d2b047f2d84907d60a2
iago <iagomauricio7@gmail.com>
2025-02-18 14:55:53 -0300

    feito o fundo de oceano

---
+++ src/app/components/bg/bg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bg',
+  standalone: true,
+  imports: [],
+  templateUrl: './bg.component.html',
+  styleUrl: './bg.component.css'
+})
+export class BgComponent {
+
+}
