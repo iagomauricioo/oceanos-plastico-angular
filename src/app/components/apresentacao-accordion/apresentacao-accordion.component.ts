6009c37ab8f2b26bf4f4464204b2a041089358a4
Iago Mauricio <iagomauricio7@gmail.com>
2025-03-14 11:00:55 -0300

    corrigindo bug do accordion que não abria ao sair da route

--- src/app/components/apresentacao-accordion/apresentacao-accordion.component.ts
+++ src/app/components/apresentacao-accordion/apresentacao-accordion.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { initAccordions } from 'flowbite';
 
 @Component({
   selector: 'app-apresentacao-accordion',
@@ -8,6 +9,8 @@ import { RouterLink } from '@angular/router';
   templateUrl: './apresentacao-accordion.component.html',
   styleUrl: './apresentacao-accordion.component.css'
 })
-export class ApresentacaoAccordionComponent {
-
+export class ApresentacaoAccordionComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    initAccordions();
+  }
 }
